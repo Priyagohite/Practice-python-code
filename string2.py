81ed1084c4d28bd83bbf9facbd18ce68f557a8ce
Pihu✨ <100400707+Priyagohite@users.noreply.github.com>
2022-04-24 13:52:00 +0530

    Add files via upload

---
+++ string2.py
@@ -0,0 +1,43 @@
+#slicing
+
+i="PRIYA"
+x=i[3:4]
+print(i)
+print(x)
+
+i="PRIYA"
+x=i[3:5]
+print(i)
+print(x)
+
+i="PRIYA"
+x=i[ :5]
+print(i)
+print(x)
+
+i="PRIYA nidhi"
+x=i[3: ]
+print(i)
+print(x)
+
+#spliting
+
+y=i.split(" ")
+print(y)
+
+#concatenation
+
+x="priya"
+y="gohite"
+#z=x+y
+z=x+" "+y
+print(x)
+print(y)
+print(z)
+
+#multiplication
+
+x="priya"
+y=2
+z= x*y
+print(z)
