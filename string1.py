c65205889c369049b7f556d2bc6cdc9b45841ba2
Pihu✨ <100400707+Priyagohite@users.noreply.github.com>
2022-04-24 13:51:40 +0530

    Add files via upload

---
+++ string1.py
@@ -0,0 +1,19 @@
+str1="I am fine"
+x=len(str1)
+
+print("length of str1=",x)
+
+#print("length of str1="+x)
+#print("length of str1={}",format(x))
+for i in range(x):
+    print(str1[i])
+
+'''
+for i in range(x):
+    print("{} is at index{}".format(str1[i].i))
+    y=str1.index("I")
+    for i in str(1):
+        print(str1.index(i))
+    for i in range(x-1,-1,-1):
+        print(str1[i])
+'''
