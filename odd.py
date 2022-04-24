56e37894bbc846697bed080ee0f36fb1992bcd7f
Pihu✨ <100400707+Priyagohite@users.noreply.github.com>
2022-04-24 13:37:16 +0530

    Add files via upload

---
+++ odd.py
@@ -0,0 +1,35 @@
+#relational operator
+
+
+a=24
+if(a%2==0):
+   print('even')
+else:
+   print('odd')
+
+
+#odd & even with float values
+a=input('Please enter the number')
+a=float(a)
+x=a%2
+print(x)
+if(a%2==0):
+   print('even')
+else:
+   print('odd')
+
+
+#odd &even with integere values
+
+a=input('Please enter the number')
+a=int(a)
+if(a==0):
+   print('zero is neither even or odd')
+elif(a%2==0):
+   print('even')
+else:
+   print('odd')
+
+
+
+
