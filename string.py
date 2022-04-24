9124b2ab05a38f78582445366fe8885d9fa0eae1
Pihu✨ <100400707+Priyagohite@users.noreply.github.com>
2022-04-24 13:50:54 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,14 @@
+#string is indent and order
+
+a=str(input("Enter ur name:"))
+print("My name is",a)
+
+#for printing str one by one
+for i in a:
+    print(i)
+
+print(a[0])
+print(a[1])
+print(a[2])
+print(a,"index")
+    
