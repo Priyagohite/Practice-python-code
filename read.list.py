ab276363b10c168d4b915feec66ef93c41ba094f
Pihu✨ <100400707+Priyagohite@users.noreply.github.com>
2022-04-26 15:41:27 +0530

    Add files via upload

---
+++ read.list.py
@@ -0,0 +1,17 @@
+#print name,addr,phno,email,clg;
+
+a=['Priya','Pithampur','9575951652','priyagoh7@gmail.com','IIPS']
+print("name=",a[0],"\naddr=",a[1],"\nphno=",a[2],"\nemail=",a[3],"\nclg=",a[4])
+
+
+b=[1,2,3,4,5,6,7]
+#array length print
+x=len(b)
+for i in range(x):
+     print(b[i])
+
+
+b=[1,2,3,4,5,6,7]
+#array length print
+x=len(b)
+print(b.index(4))
