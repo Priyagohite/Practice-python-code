23e924cb8d4cb8096539fb2bab84bfaa4007c853
Pihu✨ <100400707+Priyagohite@users.noreply.github.com>
2022-04-26 15:35:08 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,24 @@
+'''#create list
+
+a=[1,2,3,4,5]
+print(a)
+
+#or another way to create a list
+
+a=list(1)
+print(a)
+'''
+
+#insert data value at specific position
+a=[1,2,3]
+print(a)
+a.insert(2,8)
+print(a)
+
+
+#append
+
+a=[1,2,3]
+print(a)
+a.append(5)
+print(a)
