f8a36316450464d336965256d6f28e653e151217
Pihu✨ <100400707+Priyagohite@users.noreply.github.com>
2022-04-27 20:57:08 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,19 @@
+#calculate area of circle
+
+a=5
+area=22/7*a*a
+print(area)
+area=round(area,3)
+print(area)
+
+
+#user input
+
+r=int(input("Enter the radius"))
+pi=22/7
+area=pi*r*r
+print(area)
+
+
+
+
