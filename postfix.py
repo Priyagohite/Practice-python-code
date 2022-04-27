56a0a71e7ea57f47c854f1dd42fff8532825dd98
Pihu✨ <100400707+Priyagohite@users.noreply.github.com>
2022-04-27 20:51:11 +0530

    Add files via upload

---
+++ postfix.py
@@ -0,0 +1,11 @@
+n=int(input("Enter which value you want to insert:"))
+exp=[None]*n
+
+for i in range(n):
+      exp[i]=str.lower(input())
+
+stack=[None]*n
+
+str=[None]*n
+
+print("our string is")
