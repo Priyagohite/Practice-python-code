f96b60d7841343b948b53d3e2f0d13940e2684a6
Pihu✨ <100400707+Priyagohite@users.noreply.github.com>
2022-05-04 22:30:54 +0530

    Add files via upload

---
+++ nos.py
@@ -0,0 +1,30 @@
+'''a=input("enter the number:")
+for i in range(a):
+     print(i)
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
+print(a[6])
+print(a[7])
+print(a[8])
+print(a[9])
+print(a,"index")
+
+a=[1,2,3,4,5,6,7,8,9]
+length=len(a)
+for i in  range (length):
+    print(a[i])
+else:
+    print("THANKU")
+
+'''
+#for else
+
+for i in range(1,11):
+    print(i)
+else:
+    print("THANK YOU")
+
+ 
