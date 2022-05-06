69c1720f6b59b80cb8dec44514df137cae7d204b
Pihu✨ <100400707+Priyagohite@users.noreply.github.com>
2022-05-06 11:50:55 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,28 @@
+#use import keyword
+
+import math
+x=4
+y=math.sqrt(x)
+print(x)
+print(y)
+
+
+#use import_____ as keyword
+import math as m
+x=4
+y=m.sqrt(x)
+print(x)
+print(y)
+
+
+#use from____import keyword
+
+from math import sqrt
+x=5
+y=sqrt(x)
+print(x)
+print(y)
+
+
+
+
