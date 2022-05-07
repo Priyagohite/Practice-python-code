71a96967072d4ab1db2cac33874922b0758421b9
Pihu✨ <100400707+Priyagohite@users.noreply.github.com>
2022-05-07 21:42:55 +0530

    Add files via upload

---
+++ prac2.py
@@ -0,0 +1,41 @@
+#use map;
+
+a=165
+b=sum(list(map(int,str(a))))
+print(b)
+
+#Typecasting
+
+a=15
+print(type(a))
+#<class 'int'>
+#int(a)
+
+
+b=22.5
+type(b)
+#<class 'float'>
+int(b)
+
+#operators use
+
+p=4
+q=2
+r=p/q
+print(r)
+
+
+p=4
+q=2
+r=p//q
+print(r)
+
+
+a=2  # assigment operators
+print(a)
+a-=5
+print(a)
+c=3
+d=6
+print
+
